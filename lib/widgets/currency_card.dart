15a9fac043446a339d6ced50ca6b3105e4f78109
Jason Sejik Lee <gggabikr@gmail.com>
2024-06-29 17:43:38 -0700

    Update currency_card.dart

--- lib/widgets/currency_card.dart
+++ lib/widgets/currency_card.dart
@@ -4,62 +4,68 @@ class CurrencyCard extends StatelessWidget{
   
   final String name, code, amount;
   final IconData icon;
+  final bool isInverted;
+  final _blackColor = const Color.fromRGBO(31, 33, 35, 1);
+  final int order; 
 
-  const CurrencyCard({super.key, required this.name, required this.code, required this.amount, required this.icon});
+  const CurrencyCard({super.key, required this.name, required this.code, required this.amount, required this.icon, required this.isInverted, required this.order,});
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(31, 33, 35, 1),
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child:  Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Transform.translate(
+      offset: Offset(0,order <= 1 ? 0 :(order-1)*(-18)),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white: _blackColor,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child:  Padding(
+          padding: const EdgeInsets.only(top: 15, bottom: 20, left: 30, right:30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-            Text(name,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 32,
-              fontWeight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(height: 10,),
-            Row(children: [
-              const Text('amount',
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Text(name,
               style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
+                color: isInverted ? _blackColor : Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.w600,
                 ),
               ),
-              const SizedBox(width: 5,),
-              Text(code, 
-              style: const TextStyle(
-                color: Color.fromRGBO(255, 255, 255, 0.7),
-                fontSize: 20,
+              const SizedBox(height: 10,),
+              Row(children: [
+                Text(amount,
+                style: TextStyle(
+                  color: isInverted ? _blackColor : Colors.white,
+                  fontSize: 20,
+                  ),
                 ),
+                const SizedBox(width: 5,),
+                Text(code, 
+                style: TextStyle(
+                  color: isInverted ? _blackColor: Colors.white,
+                  fontSize: 20,
+                  ),
+                ),
+              ],
               ),
             ],
             ),
-          ],
-          ),
-          Transform.scale(
-            scale: 2.8,
-            child: Transform.translate(
-              offset: const Offset(8,11),
-              child: Icon((icon),
-              color: Colors.white,
-              size: 70,
+            Transform.scale(
+              scale: 2.5,
+              child: Transform.translate(
+                offset: const Offset(9,12),
+                child: Icon((icon),
+                color: isInverted ? _blackColor:Colors.white,
+                size: 70,
+                ),
               ),
             ),
+            ],
           ),
-          ],
         ),
       ),
     );
